70a569380c5f647646fa1cfdaac0b7e9f51e39a3
LinJunZheng <cocacolalinjun@163.com>
2024-05-26 21:49:59 +0800

    feat(base):业务基类使用验证器

--- app/controller/Base.php
+++ app/controller/Base.php
@@ -21,5 +21,6 @@ class Base extends BaseController
         //获取验证器
         $validate_space = "app\\validate\\{$this->controller}";
         $isTrue = $this->validate($this->request->param(), $validate_space);
+
     }
 }
