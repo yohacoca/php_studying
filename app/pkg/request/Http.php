90523f1ed155538dc02a590fa5450f8588ea2a3f
LinJunZheng <cocacolalinjun@163.com>
2024-06-06 23:59:13 +0800

    fear(请求类库):对http请求类增加属性

--- app/pkg/request/Http.php
+++ app/pkg/request/Http.php
@@ -1,24 +1,29 @@
 <?php
 namespace app\pkg\request;
 use GuzzleHttp\Client;
+use GuzzleHttp\Exception\GuzzleException;
 
 class Http
 {
-    // 保存一些请求参数
-    public array $requestParam;
+    // 地址
+    public string $url;
+    // 路径
+    public string $path;
+    // 请求头
+    public array $header;
+    // 参数
+    public array $param;
 
-    public static function url($url): Http
+    public static function url(string $url): Http
     {
         $http = new Http();
-        $http->requestParam['url'] = $url;
+        $http->url = $url;
         return $http;
     }
 
-    public function send(): array
+    public function path(string $path)
     {
-        $client = new Client();
-        $data = $client->request('');
-        return json_decode($data);
+        $http->path = $path;
     }
 
 }
