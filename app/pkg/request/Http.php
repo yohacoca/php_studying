8b09806e20a5deeea22ec14063859e3679dda8e9
LinJunZheng <cocacolalinjun@163.com>
2024-06-12 23:21:40 +0800

    feat(http请求库):实现get方法的发送

--- app/pkg/request/Http.php
+++ app/pkg/request/Http.php
@@ -1,61 +1,56 @@
 <?php
 namespace app\pkg\request;
+
 use GuzzleHttp\Client;
 
 class Http
 {
     // 地址
     public string $url;
-
     // 路径
     public string $path;
-
     // 请求头
     public array $header;
-
     // query参数
     public array $query;
-
     // 参数
     public array $param;
-
     public static function url(string $url): Http
     {
         $http = new Http();
         $http->url = $url;
         return $http;
     }
-
     public function path(string $path): Http
     {
         $this->path = $path;
         return $this;
     }
-
     public function header(array $header): Http
     {
         $this->header = $header;
         return $this;
     }
-
     public function query(array $param): Http
     {
         $this->query = $param;
         return $this;
     }
-
-
     public function param(array $param): Http
     {
         $this->param = $param;
         return $this;
     }
-
-    public function get()
+    public function get(): string
     {
-
+        $client = new Client();
+        $response = $client->get($this->url . $this->path,[
+            "header" => $this->header,
+            'query' => $this->query,
+        ]);
+        $body = $response->getBody();
+        return $body->getContents();
     }
-
     public function post()
     {
 
