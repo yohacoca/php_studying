8076576f8a2334bd1bab82016bdf15f091a4ace9
LinJunZheng <cocacolalinjun@163.com>
2024-06-14 22:33:45 +0800

    feat(文件操作库):类主体初始化

--- app/pkg/file/File.php
+++ app/pkg/file/File.php
@@ -4,5 +4,15 @@ namespace app\pkg\file;
 
 class File
 {
+    private $file;
 
+    public function __construct(string $path)
+    {
+        $this->file = fopen($path, "a+");
+    }
+
+    public function __destruct()
+    {
+        fclose($this->file);
+    }
 }
