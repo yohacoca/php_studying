5ee44d85bed76acc64df79529fc809a50f9a1326
LinJunZheng <cocacolalinjun@163.com>
2024-06-25 18:20:42 +0800

    update(排序): 优化冒泡排序

--- algorithm/base/sort/Bubble.php
+++ algorithm/base/sort/Bubble.php
@@ -4,16 +4,40 @@ namespace algorithm\base\sort;
 
 class Bubble
 {
+    // bubble 基础的冒泡排序
     public static function bubble(&$data){
         $len = count($data);
-        for ($i = 0; $i < $len - 1; $i++) {
-            for ($j = 0; $j < $len - $i - 1; $j++) {
-                if ($data[$j] > $data[$j + 1]) {
-                    $temp = $data[$j];
-                    $data[$j] = $data[$j + 1];
-                    $data[$j + 1] = $temp;
+        while ($len > 1){
+            for ($i = 0; $i < $len - 1; $i ++){
+                if($data[$i] > $data[$i + 1]){
+                    $temp = $data[$i];
+                    $data[$i] = $data[$i + 1];
+                    $data[$i + 1] = $temp;
                 }
             }
+            $len -= 1;
+        }
+    }
+
+    // bubble_2
+    // 优化添加交换变量, 若某次没有交换则证明提前排序完成
+    public static function bubble_2(&$data){
+        $isSwap = false;
+        $len = count($data);
+        while ($len > 1){
+            for ($i = 0; $i < $len - 1; $i ++){
+                if($data[$i] > $data[$i + 1]){
+                    $isSwap = true;
+                    $temp = $data[$i];
+                    $data[$i] = $data[$i + 1];
+                    $data[$i + 1] = $temp;
+                }
+            }
+            if (!$isSwap){
+                break;
+            }
+            $isSwap = false;
+            $len -= 1;
         }
     }
 
