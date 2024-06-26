2419d4c1463917d32c337bda9c808b76490d8523
LinJunZheng <cocacolalinjun@163.com>
2024-06-26 15:34:41 +0800

    feat(排序): 新增基础的选择排序

---
+++ algorithm/base/sort/Selection.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace algorithm\base\sort;
+
+class Selection
+{
+    // selection 基础的选择排序
+    public static function selection(&$data){
+        $len = count($data);
+        for ($i = 0; $i < $len; $i++) {
+            $minIndex = $i;
+            for ($j = $i + 1; $j < $len; $j++) {
+                if ($data[$j] < $data[$minIndex]) {
+                    $minIndex = $j;
+                }
+            }
+            $temp = $data[$i];
+            $data[$i] = $data[$minIndex];
+            $data[$minIndex] = $temp;
+        }
+    }
+}
