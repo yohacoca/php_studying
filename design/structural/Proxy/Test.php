316dcc5eac6b72e44005eb816623d88f295f8648
LinJunZheng <cocacolalinjun@163.com>
2024-07-16 22:13:39 +0800

    feat(设计模式): 新建代理模式单元测试文件

---
+++ design/structural/Proxy/Test.php
@@ -0,0 +1,14 @@
+<?php
+declare(strict_types=1);
+
+namespace design\structural\Proxy;
+
+use PHPUnit\Framework\TestCase;
+
+class Test extends TestCase
+{
+    public function test()
+    {
+
+    }
+}
