968218c6feeba8b20a580b56f8ff8b38d38535e3
LinJunZheng <cocacolalinjun@163.com>
2024-07-18 18:31:59 +0800

    feat(设计模式): 对代理模式的理解定义

--- design/structural/Proxy/Test.php
+++ design/structural/Proxy/Test.php
@@ -10,5 +10,15 @@ class Test extends TestCase
     public function test()
     {
 
+        // 代理模式
+        // ProxySubject 代理主题对象
+        // RealSubject 真实主题对象
+        // Subject 主题接口
+
+        // 简单来说, 代理主题和真实主题共同实现主题接口
+        // 代理主题关联真实主题
+        // 在想要调用真实主题方法的时候,
+        // 由代理主题里的同名方法去调用真是主题方法, 能够做前置准备或其它准备,
+
     }
 }
