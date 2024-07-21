c577240d9a966b5db3dc4e2e27e2c6489f945c98
LinJunZheng <cocacolalinjun@163.com>
2024-07-21 21:49:33 +0800

    feat(设计模式): 添加facade模式单元测试

---
+++ design/structural/Facade/Test.php
@@ -0,0 +1,14 @@
+<?php
+declare(strict_types=1);
+
+namespace design\structural\Facade;
+
+use PHPUnit\Framework\TestCase;
+
+class Test extends TestCase
+{
+    public function test(): void
+    {
+
+    }
+}
