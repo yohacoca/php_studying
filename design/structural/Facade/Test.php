6d741571f87e3e76e2cfbdfd68329e512dc8b30a
LinJunZheng <cocacolalinjun@163.com>
2024-07-22 20:06:57 +0800

    update(设计模式): 进一步完善facade模式单元测试

--- design/structural/Facade/Test.php
+++ design/structural/Facade/Test.php
@@ -10,5 +10,10 @@ class Test extends TestCase
     public function test(): void
     {
 
+        // 门面模式
+        // Facade 门面类
+        // SubSystem 子系统类
+
+        // 简单来说, 在门面类实现对子系统一系列操作的的封装
     }
 }
