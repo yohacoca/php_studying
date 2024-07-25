a6618a0cc9e014cdb7550d1304c2fe429ba04933
LinJunZheng <cocacolalinjun@163.com>
2024-07-25 22:40:03 +0800

    faeat(设计模式): 完成decorator模式测试

--- design/structural/Decorator/Test.php
+++ design/structural/Decorator/Test.php
@@ -7,5 +7,20 @@ use PHPUnit\Framework\TestCase;
 
 class Test extends TestCase
 {
+    public function test(): void
+    {
 
+        // 门面模式
+        // Component 抽象组件
+        // Decorator 装饰者组件
+        // ConcreteComponent 抽象装饰者组件
+        // ConcreteDecorator 具体装饰者组件
+
+
+        $decorator = new ConcreteComponent();
+        $decorator = new ConcreteDecoratorAuth($decorator);
+        $decorator->request();
+
+        $this->assertTrue(true);
+    }
 }
