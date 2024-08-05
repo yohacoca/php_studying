98fd2f71603a83ae6a355afec81b3ef25a3b7905
LinJunZheng <cocacolalinjun@163.com>
2024-08-05 22:39:56 +0800

    feat(一般业务): 新建一般业务类

---
+++ tools/business/Curl.php
@@ -0,0 +1,24 @@
+<?php
+declare(strict_types=1);
+
+namespace tools\business;
+
+class Curl
+{
+    public function list(): void
+    {
+
+    }
+    public function add(): void
+    {
+
+    }
+    public function update(): void
+    {
+
+    }
+    public function delete(): void
+    {
+
+    }
+}
