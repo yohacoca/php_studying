af1c72cb620ecabfbf540227a11c4feebe8c7a05
LinJunZheng <cocacolalinjun@163.com>
2024-08-07 22:17:13 +0800

    update(jwt): 完善Firebase JWT库的使用

--- tools/jwt/Jwt.php
+++ tools/jwt/Jwt.php
@@ -6,7 +6,7 @@ namespace tools\jwt;
 use PHPUnit\Framework\TestCase;
 use Firebase\JWT\JWT as FirebaseJWT;
 use Firebase\JWT\Key;
-use \stdClass;
+use stdClass;
 
 
 // 安装 composer require firebase/php-jwt
@@ -14,35 +14,39 @@ use \stdClass;
 class Jwt extends TestCase
 {
     public static string $key = 'KEY-TEST';
+    public static string $alg = 'HS256';
+    // (Not Before): 某个时间点后才能访问, 比如设置time+30, 表示当前时间30秒后才能使用
+    public static int $nbf = 0;
+    // 过期时间,这里设置2个小时
+    public static int $exp = 7200;
 
-    public static function encode(): string
+    // encode 加密jwt
+    public static function encode($data): string
     {
 
         $time = time();
         $token = [
-            'iss' => 'issued',      //签发者 可选
-            'aud' => 'receive',     //接收者
-            'iat' => $time,         //签发时间
-            'nbf' => $time ,        //(Not Before): 某个时间点后才能访问, 比如设置time+30, 表示当前时间30秒后才能使用
-            'exp' => $time + 7200,  //过期时间,这里设置2个小时
-            'data' => [             //自定义信息，不要定义敏感信息
-                'userid' => 1,
-                'username' => 'John'
-            ]
+            'iss' => 'issued',            // 签发者 可选
+            'aud' => 'receive',           // 接收者
+            'iat' => $time,               // 签发时间
+            'nbf' => $time + self::$nbf,
+            'exp' => $time + self::$exp,
+            'data' => $data
         ];
-        return FirebaseJWT::encode($token, self::$key, 'HS256'); //输出Token
+        return FirebaseJWT::encode($token, self::$key, self::$alg);
     }
 
-    public static function decode(string $key): stdClass
+    // decode 解密jwt
+    public static function decode(string $token): stdClass
     {
-
-        return FirebaseJWT::decode($key, new Key(self::$key, 'HS256'),);
+        return FirebaseJWT::decode($token, new Key(self::$key, self::$alg));
     }
 
     public function test()
     {
-        $jwt = $this->encode();
-        var_dump((array)$this->decode($jwt));
-        $this->assertTrue(true);
+        $data = array('user' => 'admin', 'msg' => 'test');
+        $jwt = $this->encode($data);
+        $decode = (array)$this->decode($jwt)->data;
+        $this->assertTrue($decode == $data);
     }
 }
