0bb88e300d599a8ba07e4b7573582d1fc1ed59f3
LinJunZheng <cocacolalinjun@163.com>
2024-09-08 10:51:41 +0800

    update(File): 写入到一个文件

---
+++ tools/package/File/FIle.php
@@ -0,0 +1,23 @@
+<?php
+declare(strict_types=1);
+
+namespace tools\package\File;
+
+use PHPUnit\Framework\TestCase;
+
+class FIle extends TestCase
+{
+
+    public function test_write_to_file(): void
+    {
+
+        $string = "test";
+        $file = "output.txt";
+
+        file_put_contents($file, $string);
+
+        $this->assertTrue(true);
+    }
+
+
+}
