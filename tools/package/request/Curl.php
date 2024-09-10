383d329a88863ea1c8debab9404a5e53ac98da58
LinJunZheng <cocacolalinjun@163.com>
2024-09-10 19:52:32 +0800

    update(curl): get请求

---
+++ tools/package/request/Curl.php
@@ -0,0 +1,36 @@
+<?php
+declare(strict_types=1);
+
+namespace tools\package\request;
+
+use Exception;
+use PHPUnit\Framework\TestCase;
+
+class Curl extends TestCase
+{
+
+
+    public static array $data = [
+        'user' => 'root',
+        'data' => 'success',
+    ];
+
+    public function test_get()
+    {
+        $url = 'https://example.com/api'; // 替换为目标 URL
+
+        $ch = curl_init();
+
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+        $response = curl_exec($ch);
+
+        if (curl_errno($ch)) {
+            // 抛出异常
+            throw new Exception('Curl error: ' . curl_error($ch));
+        }
+
+        return json_decode($response);
+    }
+}
