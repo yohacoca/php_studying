b5aa23c5a21f8a947a7b6ef55c152595ce772920
LinJunZheng <cocacolalinjun@163.com>
2024-10-04 17:54:15 +0800

    featt(unit test): 学习phpunit test包

---
+++ tools/package/unit/Test.php
@@ -0,0 +1,36 @@
+<?php
+declare(strict_types=1);
+
+namespace tools\package\unit;
+
+use PHPUnit\Framework\TestCase;
+
+// 安装 composer require --dev phpunit/phpunit
+
+class Test extends TestCase
+{
+    public function testPushAndPop()
+    {
+        // 初始化一个空数组，作为栈
+        $stack = [];
+
+        // 断言：检查栈的初始长度是否为 0
+        $this->assertCount(0, $stack);
+
+        // 将字符串 'foo' 推入栈中
+        $stack[] = 'foo';
+
+        // 断言：检查栈顶元素是否为 'foo'
+        // count($stack) - 1 是栈顶元素的索引，因栈的索引从 0 开始
+        $this->assertEquals('foo', $stack[count($stack) - 1]);
+
+        // 断言：检查栈的当前长度是否为 1
+        $this->assertCount(1, $stack);
+
+        // 从栈中弹出顶部元素，并断言返回值是否为 'foo'
+        $this->assertEquals('foo', array_pop($stack));
+
+        // 断言：检查栈的长度是否为 0，表示栈已为空
+        $this->assertCount(0, $stack);
+    }
+}
