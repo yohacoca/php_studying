5c4ff0b5505ef9ea39a673045a9620fccfd9e761
LinJunZheng <cocacolalinjun@163.com>
2024-10-12 00:11:14 +0800

    update(rabbitmq): 引入扩展

---
+++ tools/package/rabbitmq/RabbitMq.php
@@ -0,0 +1,13 @@
+<?php
+declare(strict_types=1);
+
+namespace tools\package\rabbitmq;
+
+use PHPUnit\Framework\TestCase;
+
+// 安装 composer require php-amqplib/php-amqplib
+
+class RabbitMq extends TestCase
+{
+
+}
