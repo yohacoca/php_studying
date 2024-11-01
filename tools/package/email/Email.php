b9bfb81a530e952a62e36e536ab5d296f505beb1
LinJunZheng <cocacolalinjun@163.com>
2024-11-01 22:44:50 +0800

    update(phpmailer): 测试发送

--- tools/package/email/Email.php
+++ tools/package/email/Email.php
@@ -3,13 +3,14 @@ declare(strict_types=1);
 
 namespace tools\package\email;
 
+use PHPMailer\PHPMailer\Exception;
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPUnit\Framework\TestCase;
 
 class Email extends TestCase
 {
 
-    function test()
+    function test_send()
     {
         //设定邮件编码,默认ISO-8859-1,如果发中文此项必须设置，否则乱码
         $mail = new PHPMailer(true);
@@ -34,32 +35,30 @@ class Email extends TestCase
 
 
         //-------------  发件人设置 -----------
-        //
-        $mail->setFrom('23426945@qq.com', '又拍云');
-        #收件人列表,可将邮件发送给多个邮箱,命令格式:
-        //$mail->addAddress('收件人邮箱', '姓名');
-        $mail->addAddress('848978691@qq.com', '李知恩');
-        $mail->addAddress('3276205785@qq.com', '灰太狼的羊');
-        #回复地址
-        $mail->addReplyTo('23426945@qq.com', '李钟硕');
-        #抄送人列表
-        $mail->addCC('itqaqcom@163.com', '你的欲梦');
-        # 邮件内容
-        //电子邮件格式设置为HTML
-        $mail->isHTML(true);
-        //邮件标题
-        $mail->Subject = '又拍云密码重置校验';
-        //邮件内容
-        $code = mt_rand(100000, 999999);
-        $mail->Body = "您的验证码为: <b>{$code}</b>";
-        //这个是设置纯文本方式显示的正文内容,如果不支持Html方式,就会用到这个,基本无用
-        $mail->AltBody = "您的验证码为: {$code}";
+        try {
+            // 发件人信息
+            $mail->setFrom('2448202116@qq.com', 'author');
+            // 收件人(可多选)
+            $mail->addAddress('cocacolalinjun@163.com', '1');
+            $mail->addAddress('cocacolalinjun@gmail.com', '2');
+            // 回复地址
+            $mail->addReplyTo('2448202116@qq.com', 'author_back');
+            // 抄送人列表
+            $mail->addCC('2448202116@163.com', 'author_addCC');
+            // 邮件格式设置为HTML
+            $mail->isHTML(true);
+            // 邮件标题
+            $mail->Subject = 'test mail';
+            # 邮件内容
+            $mail->Body = "您的验证码为: <h1>{test mail}</h1>";
+            $mail->send();
+        } catch (Exception $e) {
+            var_dump($e->getMessage());
+            var_dump($mail->ErrorInfo);
+            $this->fail();
+        }
         #发送邮件
-        $mail->send();
         echo '邮件发送成功';
-
-        // $e->getMessage() // 异常信息
-        // $mail->ErrorInfo // 邮件发送失败错误信息
-        echo "邮件发送失败: {$mail->ErrorInfo}";
+        $this->assertTrue(true);
     }
 }
